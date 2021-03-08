e4fe1d07f79da27f34a0940096472331646440b9
sirh3e <sirh3e@protonmail.com>
2021-03-08 16:59:39 +0100

    [MODIFIED] str.h added typedef for u8 adde prototype for struct str8

--- str.h
+++ str.h
@@ -1,6 +1,17 @@
 #ifndef STR_H
 #define STR_H
 
+#include<stdint.h>
+
+typedef uint8_t u8;
+
 typedef char* str;
+
+struct __attribute__ ((__packed__)) str8{
+    u8 buffer_allocated;
+    u8 buffer_length;
+    u8 buffer_flags;
+    char buffer[];
+};
 
 #endif //STR_H
