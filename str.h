1359f58cf36664c445f74b43c5209a8b23bd5ded
sirh3e <sirh3e@protonmail.com>
2021-03-08 17:05:51 +0100

    [MODIFIED] str.h added typedef for usize added

--- str.h
+++ str.h
@@ -1,11 +1,13 @@
 #ifndef STR_H
 #define STR_H
 
+#include<sys/types.h>
 #include<stdint.h>
 
 typedef uint8_t u8;
+typedef size_t  usize;
 
-typedef char* str;
+typedef char*   str;
 
 struct __attribute__ ((__packed__)) str8{
     u8 buffer_allocated;
@@ -13,5 +15,7 @@ struct __attribute__ ((__packed__)) str8{
     u8 buffer_flags;
     char buffer[];
 };
+
+
 
 #endif //STR_H
