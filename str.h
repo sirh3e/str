cf32b892014fa8b59a542c43f1a2d4605e27441b
sirh3e <sirh3e@protonmail.com>
2021-03-08 17:21:52 +0100

    [MODFIFED] str.h added function str_get_length

--- str.h
+++ str.h
@@ -9,13 +9,28 @@ typedef size_t  usize;
 
 typedef char*   str;
 
+#define STR_TYPE_STR8 1
+#define STR_TYPE_MASK 7
+
+#define STR_GET(string, type) ((struct str##type *)((string)-(sizeof(struct str##type))))
+#define STR_GET_LENGTH(string, type) (STR_GET(string, type)->buffer_length)
+
 struct __attribute__ ((__packed__)) str8{
     u8 buffer_allocated;
     u8 buffer_length;
-    u8 buffer_flags;
+    u8 flags;
     char buffer[];
 };
 
+static inline usize str_get_length(const str string){
+    u8 flags = string[-1];
 
+    switch(flags & STR_TYPE_MASK){
+        case STR_TYPE_STR8:
+            return STR_GET_LENGTH(string, 8);
+    }
+
+    return 0;
+}
 
 #endif //STR_H
