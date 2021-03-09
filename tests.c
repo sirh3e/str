b9700420744969d9d621cad5aa0c85e0832cb1f5
sirh3e <sbluewin.ch>
2021-03-09 18:28:34 +0100

    [MODIFIED] fixed tests.c now looks like tests

--- tests.c
+++ tests.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
 #include "str.h"
 
+#define STR_TEST_COLOR_REST "\033[0m"
+#define STR_TEST_COLOR_RED "\033[31m"
+#define STR_TEST_COLOR_GREEN "\033[32m"
+
+#define TEST_ASSERT_MESSAGE(expression, message) \
+            printf("[ %s%s%s ] file: [%s] function: [%s] line: [%d] [%s]\n", \
+            expression ? STR_TEST_COLOR_GREEN : STR_TEST_COLOR_RED, expression ? "PASSED" : "FAILED", STR_TEST_COLOR_REST, \
+            __FILE__, __func__, __LINE__, message)
+
+#define TEST_ASSERT(expression) \
+           TEST_ASSERT_MESSAGE(expression, #expression)
+
 int main(int argc, char** argv){
 
+    usize capacity = 127;
     str string;
-    if((string = str_with_capacity(127)) == NULL){
+    if((string = str_with_capacity(capacity)) == NULL){
         exit(1);
     }
-
-    printf("%s allocated: %zu length: %zu\n", string, str_get_allocated(string), str_get_length(string));
-    printf("%d\n", str_get_flags(string));
-
-    str_set_flags(string, 169);
-    printf("%d\n", str_get_flags(string));
+    TEST_ASSERT(string != NULL);
+    TEST_ASSERT(str_get_allocated(string) == capacity + 1);
+    TEST_ASSERT(str_get_length(string) == 0);
+    TEST_ASSERT(str_get_flags(string) == (STR_TYPE_STR8 & STR_TYPE_MASK));
 
     return 0;
 }
