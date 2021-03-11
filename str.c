0c1d3d25ccaa91c6b5c57ecceb451c9aa9cb9604
sirh3e <sirh3e@protonmail.com>
2021-03-11 17:04:57 +0100

    [MODIFIED] str.c added helper function helper_get_origin_ptr from str address

--- str.c
+++ str.c
@@ -7,7 +7,17 @@
 #define ASSERT(expression) \
     assert(expression)
 
+void* helper_get_origin_ptr(str self);
 int helper_is_char_boundry(str self, usize index);
+
+void* helper_get_origin_ptr(str self){
+    ASSERT(self);
+
+    u8 type = str_get_type(self);
+    usize struct_size = str_get_struct_size(type);
+
+    return self - struct_size;
+}
 
 int helper_is_char_boundry(str self, usize index){
     ASSERT(self);
