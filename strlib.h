0586b5450a5c5eced5069341338b7e954a349d02
sirh3e <sirh3e@protonmail.com>
2021-03-11 20:17:51 +0100

    [MODIFIED] str.c, str.h str_*loc -> strlib_*loc, str_free in strlib.h -> strlib_free avoid conflict of naming function from str.h str_free

--- strlib.h
+++ strlib.h
@@ -1,8 +1,8 @@
 #ifndef STR_STRLIB_H
 #define STR_STRLIB_H
 
-#define str_malloc  malloc
-#define str_realloc realloc
-#define str_free    free
+#define strlib_malloc  malloc
+#define strlib_realloc realloc
+#define strlib_free    free
 
 #endif //STR_STRLIB_H
