ffb636177b5bf10264a25e5cd383e90c34ca94c1
sirh3e <sirh3e@protonmail.com>
2021-03-12 03:13:13 +0100

    [MODIFIED] str.c refactored tests.c CURRNELTY BROKEN

--- tests.c
+++ tests.c
@@ -1,46 +1,45 @@
 #include <stdio.h>
 #include "str.h"
 
-#define STR_TEST_COLOR_REST "\033[0m"
-#define STR_TEST_COLOR_RED "\033[31m"
-#define STR_TEST_COLOR_GREEN "\033[32m"
+#define STR_TEST_COLOR_REST     "\033[0m"
+#define STR_TEST_COLOR_RED      "\033[31m"
+#define STR_TEST_COLOR_GREEN    "\033[32m"
 
-#define TEST_ASSERT_MESSAGE(expression, message)                                \
-            printf("[ %s%s%s ] file: [%s] function: [%s] line: [%d] [%s]\n",    \
-                expression ? STR_TEST_COLOR_GREEN : STR_TEST_COLOR_RED,	        \
-                expression ? "PASSED" : "FAILED", STR_TEST_COLOR_REST,          \
-                __FILE__, __func__, __LINE__,                                   \
-                message                                                         \
+#define TEST_ASSERT_MESSAGE(left, right, message)                                                                     \
+            printf("[ %s%s%s ] file: [%s] function: [%s] line: [%d] expression: [%ld %s %ld] values: [%ld %s %ld] message: [%s]\n", \
+                left == right ? STR_TEST_COLOR_GREEN : STR_TEST_COLOR_RED,                                                \
+                left == right ? "PASSED" : "FAILED", STR_TEST_COLOR_REST,                                                 \
+                __FILE__, __func__, __LINE__,                                                                                   \
+                #left, "==", #right,                                                                                       \
+                left, "==", right,                                                                                          \
+                message                                                                                                         \
             )
 
-#define TEST_ASSERT(expression) \
-           TEST_ASSERT_MESSAGE(expression, #expression)
+#define TEST_ASSERT_EQ(left, right) \
+    TEST_ASSERT_MESSAGE(left, right, "")                   
+
+#define TEST(string, capacity, length, type)                        \
+    TEST_ASSERT(string);                                            \
+    TEST_ASSERT_EQ(capacity, str_get_capacity(string));             \
+    TEST_ASSERT_EQ(length, str_get_length(string));                 \
+    TEST_ASSERT_EQ((type & STR_TYPE_MASK), str_get_flags(string)) 
 
 int main(int argc, char** argv){
 
-    usize capacity = 127;
-    str string = str_with_capacity(capacity); 
+    usize _capacity = 127;
+    usize _length = 0;
     
-    TEST_ASSERT(string);
-    TEST_ASSERT(str_get_capacity(string) == capacity + 1);
-    TEST_ASSERT(str_get_length(string) == 0);
-    TEST_ASSERT(str_get_flags(string) == (STR_TYPE_STR8 & STR_TYPE_MASK));
-
+    //str string = str_with_capacity(_capacity); 
+    str string = NULL;
+    TEST(string, _capacity, _length, STR_TYPE_STR8);
+    /*
     str_clear(string);
+    TEST(string, capacity, length, STR_TYPE_STR8);
 
-    TEST_ASSERT(string);
-    TEST_ASSERT(str_get_capacity(string) == capacity + 1);
-    TEST_ASSERT(str_get_length(string) == 0);
-    TEST_ASSERT(str_get_flags(string) == (STR_TYPE_STR8 & STR_TYPE_MASK));
-
-    //ToDo str_free
-    capacity = 4; 
-    string = str_new("sirh3e", capacity - 1);
-
-    TEST_ASSERT(string);
-    TEST_ASSERT(str_get_capacity(string) == capacity);
-    TEST_ASSERT(str_get_length(string) == 3);
-    TEST_ASSERT(str_get_flags(string) == (STR_TYPE_STR8 & STR_TYPE_MASK));
-
+    capacity = 4;
+    length = capacity;
+    string = str_new("sirh3e", length);
+    TEST(string, capacity, length, STR_TYPE_STR8);
+    */
     return 0;
 }
