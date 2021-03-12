700a3dc92c18ede56fff4f6e4f3a7a3298cb408b
sirh3e <sirh3e@protonmail.com>
2021-03-12 03:52:42 +0100

    [MODIFIED] str.c refactored tests.c fixed bug

--- tests.c
+++ tests.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include "str.h"
 
@@ -5,18 +6,20 @@
 #define STR_TEST_COLOR_RED      "\033[31m"
 #define STR_TEST_COLOR_GREEN    "\033[32m"
 
-#define TEST_ASSERT_MESSAGE(left, right, message)                                                                     \
-            printf("[ %s%s%s ] file: [%s] function: [%s] line: [%d] expression: [%ld %s %ld] values: [%ld %s %ld] message: [%s]\n", \
-                left == right ? STR_TEST_COLOR_GREEN : STR_TEST_COLOR_RED,                                                \
-                left == right ? "PASSED" : "FAILED", STR_TEST_COLOR_REST,                                                 \
-                __FILE__, __func__, __LINE__,                                                                                   \
-                #left, "==", #right,                                                                                       \
-                left, "==", right,                                                                                          \
-                message                                                                                                         \
+#define TEST_ASSERT_MESSAGE_EQ(left_string, left_value, right_string, right_value)                                  \
+            printf("[ %s%s%s ] file: [%s] function: [%s] line: [%d] expression: [%s %s %s] values: [%lu %s %lu]\n", \
+                left_value == right_value ? STR_TEST_COLOR_GREEN : STR_TEST_COLOR_RED,                              \
+                left_value == right_value ? "PASSED" : "FAILED", STR_TEST_COLOR_REST,                               \
+                __FILE__, __func__, __LINE__,                                                                       \
+                #left_string, "==", #right_string,                                                                  \
+                left_value, "==", right_value                                                                       \
             )
 
+#define TEST_ASSERT(expression) \
+    assert(expression)
+                
 #define TEST_ASSERT_EQ(left, right) \
-    TEST_ASSERT_MESSAGE(left, right, "")                   
+    TEST_ASSERT_MESSAGE_EQ(#left, left, #right, right) 
 
 #define TEST(string, capacity, length, type)                        \
     TEST_ASSERT(string);                                            \
@@ -26,13 +29,12 @@
 
 int main(int argc, char** argv){
 
-    usize _capacity = 127;
-    usize _length = 0;
+    usize capacity = 127;
+    usize length = 0;
     
-    //str string = str_with_capacity(_capacity); 
-    str string = NULL;
-    TEST(string, _capacity, _length, STR_TYPE_STR8);
-    /*
+    str string = str_with_capacity(capacity); 
+    TEST(string, capacity, length, STR_TYPE_STR8);
+    
     str_clear(string);
     TEST(string, capacity, length, STR_TYPE_STR8);
 
@@ -40,6 +42,6 @@ int main(int argc, char** argv){
     length = capacity;
     string = str_new("sirh3e", length);
     TEST(string, capacity, length, STR_TYPE_STR8);
-    */
+    
     return 0;
 }
